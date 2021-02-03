653bce4c58a007d2fe91c58618e8bf536de007c8
Sinjin Swanepoel <sswanepoel@2u.com>
2021-02-03 17:36:44 +0200

    Fix script and readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# bb-kubectl-wrappers
-Babashka Helpers for kubectl
+## To Run
+
+### Get all pods
+./bb-kube.clj -v
+### by Node
+./bb-kube.clj -vn # List all Nodes
+./bb-kube.clj -vn ip-123-123-123-123.aws-region-1.compute.internal # Filter by specific Node
+
+### Compare feature branches
+./bb-kube.clj -vr path/to/repo
--- bb-kube.clj
+++ bb-kube.clj
@@ -1,6 +1,11 @@
 #!/usr/bin/env bb
 
-(require '[clojure.java.shell :as sh])
+(ns bb-kube
+  (:require
+   [clojure.string :as str]
+   [clojure.java.io :as io]
+   [clojure.java.shell :as sh]
+   [clojure.tools.cli :as tc]))
 
 (defn clever-split [keys out]
   (for
@@ -9,9 +14,17 @@
      keys
      (str/split l #"\s+"))))
 
+;; ------- ;;
+;; kubectl ;;
+;; ------- ;;
+
+(defn add-pod-info [pods pod-key]
+  {:namespace pod-key
+   :pod-info (get pods pod-key)})
+
 ; "kubectl get pods --all-namespaces -o wide --sort-by=" {.spec.nodeName} "
 
-(defn get-pods []
+(defn list-pods []
   (println "fetching pods...")
   (let [{:keys [out]}
         (->>
@@ -24,107 +37,205 @@
      [:namespace :name :ready :status :restarts :age :ip :node :nominated-node :readiness-gates]
      out)))
 
+(defn get-pods []
+  (let [pods
+        (list-pods)
+
+        grouped-pods
+        (group-by :namespace pods)]
+
+    (map
+     (partial add-pod-info grouped-pods)
+     (keys grouped-pods))))
+
+;; ------- ;;
+;;   git   ;;
+;; ------- ;;
+
+(defn branch-matches?
+  [{:keys [namespace] :as pod}
+   {:keys [] :as branch}]
+
+  (re-matches
+   (re-pattern (str ".*" namespace))
+   (get branch :name)))
+
+(defn add-branch-info [branches pod]
+  (let
+   [matched-branches
+    (filter (partial branch-matches? pod) branches)]
+
+    (if (pos? (count matched-branches))
+      (assoc pod :branches matched-branches)
+      pod)))
+
 ; git ls-remote --heads origin
 
-(defn get-branches []
+(defn get-branches [repo-path]
   (println "fetching branches...")
   (let [{:keys [out]}
-        (->>
-         ["git" "ls-remote" "--heads" "origin"]
-         (apply sh/sh))]
-    
+        (sh/with-sh-dir repo-path
+          (->>
+           ["git" "ls-remote" "--heads" "origin"]
+           (apply sh/sh)))]
+
     (clever-split
      [:id :name]
      out)))
 
-(defn in-branch [kube-namespace branch]
-  (let
-   [matches
-    (re-matches
-     (re-pattern (str ".*" kube-namespace))
-     (get branch :name))]
-    
-    ;(println "Matches? " (str ".*" kube-namespace) (get branch :name) (count matches) matches)
 
-    (if (>= (count matches) 1)
-      {:matched true
-       :namespace kube-namespace
-       :branch-id (:id branch)
-       :branch-name (:name branch)}
-      
-      {:matched false
-       :namespace kube-namespace})))
+;; ------- ;;
+;;  print  ;;
+;; ------- ;;
 
-(defn namespace-in-branch [branches kube-namespace]
-  (set
-   (map
-    (partial in-branch kube-namespace)
-    branches)))
 
-(defn add-pod-info [pod-keys pods pod-branch]
-  (merge
-   pod-branch
-   {:pod-info
-    (map
-     #(select-keys % pod-keys)
-     (get pods (:namespace pod-branch)))}))
-
-(defn with-pod-info [pod-keys grouped-pods matched-namespaces]
-  (map
-   (partial add-pod-info pod-keys grouped-pods)
-   matched-namespaces))
-
-(defn print-pods [pods message & [sort-fn]]
+(defn print-pods [{:keys [verbosity]} pods message & [sort-fn]]
   (println "")
   (println "---------------")
   (println message)
   (println "---------------")
-
+  
   (doseq
-   [{:keys [namespace pod-info branch-name]}
+   [{:keys [namespace pod-info branches]}
     (sort-by (if sort-fn sort-fn :namespace) pods)]
 
-    (println
-     namespace
-     "-" (count pod-info)
-     "-" (set (map #(get % :age) pod-info))
-     (if branch-name (str "- " branch-name) ""))
-    
-    (doseq [p (sort-by :name pod-info)]
-      (println "  " (:name p) "-" (:age p)))))
+    (let
+     [fn-print
+      (fn []
+        (println
+         namespace
+         "-" (count pod-info)
+         "-" (set (map #(get % :age) pod-info))
+         (if branches (str/join ", " (map :name branches)) "")))]
 
-(defn -main []
-  (let [pods
-        (get-pods)
+      (cond
+        (> verbosity 0)
+        (do
+          (fn-print)
+          (doseq [p (sort-by :name pod-info)]
+            (println "  " (:name p) "-" (:age p) "-" (:node p))))
 
-        grouped-pods
-        (group-by :namespace pods)
+        :else
+        (fn-print)))))
 
-        kube-namespaces
-        (->>
-         pods
-         (map (fn [pod] (get pod :namespace)))
-         set)
+(defn single-node [{:keys [verbosity]} node pods sort-fn]
+  (let
+   [fn-print
+    (fn []
+      (println
+       node
+       "-" (count pods)))]
 
-        branches
-        (->>
-         (get-branches))
+    (cond
+      (> verbosity 0)
+      (do
+        (fn-print)
+        (doseq [{:keys [namespace name age]}
+                (sort-by (if sort-fn sort-fn (juxt :namespace :name :age)) pods)]
+          (println "  " namespace "-" name "-" age)))
+
+      :else
+      (fn-print))))
+
+(defn print-nodes [{:keys [verbosity] :as options} nodes message & [sort-fn]]
+  (when message
+    (println "")
+    (println "---------------")
+    (println message)
+    (println "---------------"))
+  
+  (cond
+    (map? nodes)
+    (doseq
+     [[node pods] nodes]
+      (single-node options node pods sort-fn))
+
+    :else
+    (single-node options (get-in nodes [0 :node]) nodes sort-fn)))
+
+;; ------- ;;
+;;  -main  ;;
+;; ------- ;;
+
+(def cli-options [["-r" "--repo" ""]])
+
+(def cli-options
+  ;; An option with a required argument
+  [["-r" "--repo REPO" "Pick a repo to compare it to"
+    :parse-fn #(.getAbsolutePath (io/file %))]
+
+   ["-n" "--nodes NODE" "Filter by nodes"
+    :default true
+    :parse-fn #(str %)]
+
+   ["-v" nil "Verbosity level"
+    :id :verbosity
+    :default 0
+    :update-fn inc]
+
+   ;; A boolean option defaulting to nil
+   ["-h" "--help"]])
+
+(defn -main [& args]
+  (let [{:keys [summary options arguments]}
+        (tc/parse-opts args cli-options)]
+
+    (cond
+      (:help options)
+      (do
+        (println "Help")
+        (println summary))
+
+      :else
+      (let [pods (get-pods)]
+        (cond
+
+          (:repo options)
+          (let
+           [branches
+            (get-branches (:repo options))
+
+            pods
+            (->>
+             pods
+             (map (partial add-branch-info branches)))
+
+            [with-branches
+             without-branches]
+            [(filter #(get % :branches) pods)
+             (remove #(get % :branches) pods)]]
+
+            (println "namespaces: " (count pods))
+            (println "branches: " (count branches))
+
+            (print-pods options with-branches "PODs with existing branches")
+            (print-pods options without-branches "PODs without existing branches"))
+
+          (:nodes options)
+          (let
+           [node-name (:nodes options)
+            actual-pods
+            (mapcat
+             #(get % :pod-info)
+             pods)
+
+            by-node
+            (group-by :node actual-pods)]
+
+            (if (true? (:nodes options))
+              (print-nodes options by-node "Nodes")
+              (print-nodes options (get by-node node-name) (str "Node: " node-name))))
 
 
+          :else
+          (do
+            (println "namespaces: " (count pods))
+            (print-pods options pods "Running PODs")))))))
 
-        create-list
-        (->>
-         kube-namespaces
-         (mapcat (partial namespace-in-branch branches))
-         (with-pod-info [:name :ip :age :node] grouped-pods))
 
-        [with-branches without-branches]
-        [(filter #(get % :matched) create-list) (remove #(get % :matched) create-list)]]
+(apply -main *command-line-args*)
 
-    (println "namespaces: " (count kube-namespaces))
-    (println "branches: " (count branches))
+;;;; Scratch
 
-    (print-pods with-branches "PODs with existing branches")
-    (print-pods without-branches "PODs without existing branches")))
-
-(-main)
+(comment
+  (-main "-r repo" "-n namespace"))
